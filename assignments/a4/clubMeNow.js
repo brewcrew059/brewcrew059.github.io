0cde712e321de920e8511c638bd808c90ddb139e
brewcrew059 <47646708+brewcrew059@users.noreply.github.com>
2021-10-24 12:40:44 -0700

    Update clubMeNow.js

--- assignments/a4/clubMeNow.js
+++ assignments/a4/clubMeNow.js
@@ -91,7 +91,7 @@ function displayClubDistanceEntryForm(c) {
 
 // replace the current "clubs" array with the previous one
 function undoLastShot() {
-        // your code here !
+        clubs = JSON.parse(localStorage.getItem("clubsUndo"));
 }
 
 // create a new (default) "clubs" array
