f5a03258d4896d7486526bbc3e797d00d881878a
brewcrew059 <47646708+brewcrew059@users.noreply.github.com>
2021-10-25 18:22:30 -0700

    Update partB.js

--- assignments/a4/partB.js
+++ assignments/a4/partB.js
@@ -40,8 +40,8 @@ appendTableRow3(table2a,"7","8","9");
 let div2b = document.getElementById("2B");
 let table2b = createTable("table2b");
 div2b.appendChild(table2b);
-table2a.setAttribute("style", "border:1px solid black;")
-table2a.setAttribute("width", "100%")
+table2b.setAttribute("style", "border:1px solid black;")
+table2b.setAttribute("width", "100%")
 appendTableRow5(table2b,"1","2","3","4","5");
 appendTableRow5(table2b,"6","7","8","9","10");
 appendTableRow5(table2b,"11","12","13","14","15");
